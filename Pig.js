2e305c3aa84f8deee1a64e3dd98f2813473edd2f
shield10724s <71579406+shield10724s@users.noreply.github.com>
2020-12-12 18:06:08 +0530

    Add files via upload

--- Pig.js
+++ Pig.js
@@ -18,11 +18,14 @@ class Pig extends BaseClass {
      image(this.image, this.body.position.x, this.body.position.y, 50, 50);
      pop();
    }
- }
+  }
 
- score(){
-   if(this.Visibility<0 && this.Visiblity>-1005){
-     score++;
-   }
- }
+  score(){
+    if (this.Visiblity < 0 && this.Visiblity > -1005){
+      score++;
+    }
+  }
+
+
+
 };
